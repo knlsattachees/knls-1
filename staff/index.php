9aa02e1d44129df3d7743ec0cccc8b530cdbd96e
knlsattachees <knlsattachees@gmail.com>
2024-07-17 03:08:17 -0700

    Add files via upload

--- staff/index.php
+++ staff/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 if (isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header("Location: dashboard.php");
     exit();
 } else {
-    header("location: dashboard.php");
+    header("location: login.php");
     exit();
 }
